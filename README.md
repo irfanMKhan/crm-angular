551a261379649f129a13a5b82a5ae36e7ec6c541
irfanMKhan <irfanupdate@yahoo.com>
2025-09-15 23:58:39 -0400

    Update application name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularInterview
+# CRM
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.1.
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,6 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +13,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideStore(),
     provideEffects(),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 };
